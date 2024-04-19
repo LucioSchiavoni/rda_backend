d020d1794452630d7abea17bfb9b423840201e78
lucioschiavoni <luciosc1798@gmail.com>
2024-04-19 09:14:24 -0300

    fix: express urlencoded

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import notasRouter from './src/routes/notas.routes.js'
 import userRouter from './src/routes/user.routes.js'
 import dotenv from 'dotenv'
-import bodyParser from 'body-parser'
+
 dotenv.config()
 
 
@@ -11,8 +11,6 @@ const app = express()
 
 const PORT = process.env.PORT
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
 const opcionesCors = {
     origin: process.env.FRONTEND_URL_DEV,
@@ -20,15 +18,17 @@ const opcionesCors = {
 };
 
 app.use(cors(opcionesCors))
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use("/upload", express.static("src/middlewares/upload"))
 app.use("/", notasRouter)
 app.use("/", userRouter)
 
 app.get("/", (req,res) => {
-    res.send("Index")
+    res.json("Index")
+
 })
 
 app.listen(PORT, () =>  { 
-    console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`)
+    console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })
